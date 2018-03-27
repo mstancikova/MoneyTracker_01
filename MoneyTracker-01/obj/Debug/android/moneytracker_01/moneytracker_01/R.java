d55bb4c34d0fd5df8415eb28ab87548f4a3382e9
mstancikova@gmail.com <AlMika!1976>
2018-03-27 17:05:22 +0200

    modifiche a casa 27/03/2018

--- MoneyTracker-01/obj/Debug/android/moneytracker_01/moneytracker_01/R.java
+++ MoneyTracker-01/obj/Debug/android/moneytracker_01/moneytracker_01/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
+        public static final int bankList=0x7f040005;
         public static final int btnBanks=0x7f04000c;
         public static final int btnExpencies=0x7f04000a;
         public static final int btnGroups=0x7f04000b;
@@ -20,15 +21,14 @@ public final class R {
         public static final int edtDate=0x7f040000;
         public static final int edtGroupname=0x7f040007;
         public static final int edtMoney=0x7f040002;
+        public static final int groupList=0x7f040006;
         public static final int listViewBanks=0x7f040004;
         public static final int listViewGroups=0x7f040009;
-        public static final int txtBank=0x7f040005;
-        public static final int txtGroupname=0x7f040006;
     }
     public static final class layout {
         public static final int banks=0x7f020000;
-        public static final int bankslist=0x7f020001;
-        public static final int grouplist=0x7f020002;
+        public static final int blist=0x7f020001;
+        public static final int glist=0x7f020002;
         public static final int groups=0x7f020003;
         public static final int main=0x7f020004;
     }
